db4483b4e918dfb09a015952d54d26cbb53da39f
MacBook <macbook@daniels-mbp.home>
2020-05-11 11:50:52 -0700

    Change to desmostylian

---
+++ script.js
@@ -0,0 +1,103 @@
+var imageWidth = 1085,
+    imageHeight = 505,
+    imageAspectRatio = imageWidth / imageHeight,
+    $window = $(window);
+
+var hotSpots = [{
+  'title': 'Tusks',
+  'description': 'Desmostylians used their tusks to rip up seagrass and sucked them up like a vacuum!',
+  'x': -475,
+  'y': -20
+}, {
+  'title': 'Unusual body plan',
+  'description': 'Desmostylians either walked on the shallow ocean floor like a hippo or actively swam like a sea lion.',
+  'x': 275,
+  'y': -50 
+}, {
+  'title': 'Legs for swimming?',
+  'description': 'It might not look like it, but these legs were well adapted for a mainly aquatic lifestyle.',
+  'x': -200,
+  'y': 30
+}];
+
+function appendHotSpots() {
+  for (var i = 0; i < hotSpots.length; i++) {
+    var $hotSpot = $('<div>').addClass('hot-spot');
+    $('.container').append($hotSpot);
+  }
+  positionHotSpots();
+}
+
+function appendSpeechBubble() {
+  var $speechBubble = $('<div>').addClass('speech-bubble');
+  $('.container').append($speechBubble);
+}
+
+function handleHotSpotMouseover(e) {
+  var $currentHotSpot = $(e.currentTarget),
+      currentIndex = $currentHotSpot.index(),
+      $speechBubble = $('.speech-bubble'),
+      title = hotSpots[currentIndex]['title'],
+      description = hotSpots[currentIndex]['description'],
+      hotSpotTop = $currentHotSpot.offset().top,
+      hotSpotLeft = $currentHotSpot.offset().left,
+      hotSpotHalfSize = $currentHotSpot.width() / 2,
+      speechBubbleHalfSize = $speechBubble.width() / 2,
+      topTarget = hotSpotTop - $speechBubble.height(),
+      leftTarget = (hotSpotLeft - (speechBubbleHalfSize)) + hotSpotHalfSize;
+  
+  $speechBubble.empty();
+  $speechBubble.append($('<h1>').text(title));
+  $speechBubble.append($('<p>').text(description));
+  
+  $speechBubble.css({
+    'top': topTarget - 20,
+    'left': leftTarget,
+    'display': 'block'
+  }).stop().animate({
+    opacity: 1
+  }, 200);
+}
+
+function handleHotSpotMouseout(){
+  var $speechBubble = $('.speech-bubble');
+  $speechBubble.stop().animate({
+    opacity: 0
+  }, 200, function(){
+    $speechBubble.hide();
+  });
+}
+
+function positionHotSpots() {
+  var windowWidth = $window.width(),
+    windowHeight = $window.height(),
+    windowAspectRatio = windowWidth / windowHeight,
+    $hotSpot = $('.hot-spot');
+
+  $hotSpot.each(function(index) {
+    var xPos = hotSpots[index]['x'],
+        yPos = hotSpots[index]['y'],
+        desiredLeft = 0,
+        desiredTop = 0;
+
+    if (windowAspectRatio > imageAspectRatio) {
+      yPos = (yPos / imageHeight) * 100;
+      xPos = (xPos / imageWidth) * 100;
+    } else {
+      yPos = ((yPos / (windowAspectRatio / imageAspectRatio)) / imageHeight) * 100;
+      xPos = ((xPos / (windowAspectRatio / imageAspectRatio)) / imageWidth) * 100;
+    }
+
+    $(this).css({
+      'margin-top': yPos + '%',
+      'margin-left': xPos + '%'
+    });
+
+  });
+}
+
+appendHotSpots();
+appendSpeechBubble();
+$(window).resize(positionHotSpots);
+$('.hot-spot').on('mouseover', handleHotSpotMouseover);
+$('.hot-spot').on('mouseout', handleHotSpotMouseout);
